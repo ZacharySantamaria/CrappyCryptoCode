9559b6fc7e71dabb4e171a6a8f763c90f4dbeb1d
Zachary Santamaria <ZSantamaria@protonmail.com>
2018-11-27 21:18:09 -0500

    Moving things into functions

--- main.py
+++ main.py
@@ -5,26 +5,38 @@ import time
 #We should add other crypto coins in functions
 #formatt in a better way.
 
+def bitcoin(content):
+	#prints very specifc portion of the array to find the price
+	print('the value of ' + content[1][1:4]+ ' is '+ content[1][27:35])
 
-while True:
+def zcash():
+
+	pass
+
+def loadPage():
+
 	#Grabbing url with requests
 	page = requests.get('https://www.coinmarketcap.com')
-	
-	#Sending page to Bs4 to parse info
+        
+        #Sending page to Bs4 to parse info
 	soup = bs4(page.text, 'html.parser')
-	
+        
 	divs = soup.findAll('table', id='currencies')
-	
+        
 	content = []
-	
-	#finds all div tags and loops through them
+        
+        #finds all div tags and loops through them
 	for div in divs:
 		rows = div.findAll('tr')
 		for row in rows:
-			#looping through all the row in the singular div
+        		#looping through all the row in the singular div
 			content.append(row.text.replace('\n', '')[:-115])
 	
-	#prints very specifc portion of the array to find the price
-	print('the value of ' + content[1][1:4]+ ' is '+ content[1][27:35])
+	return content
+
+while True:
+	
+	content = loadPage()
+	bitcoin(content)
 	time.sleep(60)	
 	
