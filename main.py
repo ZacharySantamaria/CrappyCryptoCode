928f954254c0ffd1464e279744eddb00460396a1
Zachary Santamaria <ZSantamaria@protonmail.com>
2018-11-28 14:24:17 -0500

    added Monero function... Should probably stop saying function or module and use the proper term... Well its done.

--- main.py
+++ main.py
@@ -2,34 +2,31 @@ import requests
 from bs4 import BeautifulSoup as bs4
 import time
 
-#We should add other crypto coins in functions
-#formatt in a better way.
-
 def bitcoin(content):
 	#prints very specifc portion of the array to find the price
-	print('the value of ' + content[1][1:4]+ ' is '+ content[1][27:35])
+	print('the value of ' + content[1][1:4]+ ' is '+ content[1][26:34])
 
-def zcash():
-
-	pass
+def Monero(content):
+	#print very specific portion of the content array
+	print('The value of ' + content[11][2:5] + ' is ' + content[11][25:31])
 
 def loadPageCM():
-
 	#Grabbing url with requests
 	page = requests.get('https://www.coinmarketcap.com')
         
-        #Sending page to Bs4 to parse info
+    #Sending page to Bs4 to parse info
 	soup = bs4(page.text, 'html.parser')
         
 	divs = soup.findAll('table', id='currencies')
         
 	content = []
         
-        #finds all div tags and loops through them
+    #finds all div tags and loops through them
 	for div in divs:
 		rows = div.findAll('tr')
 		for row in rows:
-        		#looping through all the row in the singular div
+     		#looping through all the row in the singular div
+			#appending to content array and removing the ending portion
 			content.append(row.text.replace('\n', '')[:-115])
 	
 	return content
@@ -38,5 +35,6 @@ while True:
 	
 	content = loadPageCM()
 	bitcoin(content)
+	Monero(content)
 	time.sleep(60)	
 	
